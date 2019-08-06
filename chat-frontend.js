09d768b1b6f4cd46cc62d869d5d910213d069b89
Alisamar Husain <zrthxn@ip-172-31-5-165.ap-south-1.compute.internal>
2019-08-06 13:03:15 +0000

    two

--- chat-frontend.js
+++ chat-frontend.js
@@ -24,7 +24,7 @@ $(function () {
     }
 
     // open connection
-    var connection = new WebSocket('ws://127.0.0.1:1337');
+    var connection = new WebSocket('ws://13.233.155.228:1337');
 
     connection.onopen = function () {
         // first we want users to enter their names
